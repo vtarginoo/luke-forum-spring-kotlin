17c4b3c690f4cfa795972b1fa46ce023003d783b
vtarginoo <vtarginoo>
2024-09-06 19:45:23 -0300

    first commit

---
+++ src/main/kotlin/br/luke/luke_forum/LukeForumApplication.kt
@@ -0,0 +1,11 @@
+package br.luke.luke_forum
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LukeForumApplication
+
+fun main(args: Array<String>) {
+	runApplication<LukeForumApplication>(*args)
+}
---
+++ src/test/kotlin/br/luke/luke_forum/LukeForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.luke.luke_forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LukeForumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
